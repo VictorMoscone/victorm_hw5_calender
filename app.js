b2addbf5fea3c0d86b4a6b502f087a5bbc428a4d
Victor Moscone <victormoscone@gmail.com>
2020-10-30 20:20:49 -0700

    Feat: Added a system that detects the time relative to each labeled box in order to change their color as needed. Further improvements will be required.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 $(document).ready(function (){
     $("#currentDay").text(`${moment().format('dddd')}, ${moment().format('MMMM Do YYYY')}`);
     // TODO: Will need to use military time when trying to sense if the even has passed or not.
+    if (moment().format('H') > 9) {
+        $("#9am-hourBox").addClass("past");
+    }
 })
