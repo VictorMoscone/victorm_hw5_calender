0e51bf11e580b2ce623d3e0516638903e2f26f57
Victor Moscone <victormoscone@gmail.com>
2020-10-31 17:05:16 -0700

    Feat: The save button now saves inputs for the respective input box to local storage.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function (){
     $("#currentDay").text(`${moment().format('dddd')}, ${moment().format('MMMM Do YYYY')}`);
     $(".hourBox").each(function () {
         // All of the text boxes for each hour will be checked on refresh.
-        let theHour = this.getAttribute("id")
+        let theHour = $(this).attr("id")
         if (moment().format('H') > parseInt(theHour)) {
             // If the time of the box has passed...
             $(this).addClass("past");
@@ -15,10 +15,20 @@ $(document).ready(function (){
             $(this).addClass("future");
             // otherwise, it will be colored (green) as in the future.
         }
-        
     })
-    // if (moment().format('H') > 20) {
-    //     $("#9").addClass("past");
-    // }
-// TODO: .each for each hourBox that detects the time. If it's past, present, future.
+    $(".saveBtn").each(function () {
+        let buttonHour = $(this).attr("id");
+        // Find's the button's unique ID.
+        let boxHour = parseInt(buttonHour);
+        // Convert's the button's ID to a number type.
+        $(this).on("click", function(){
+            // on button click...
+            let textEntered = JSON.stringify($(`#${boxHour}`).val());
+            // This finds input for the box of the same number that the button was (9am box, 9am button).
+            localStorage.setItem(`${boxHour}-input`, textEntered); 
+            // saves the input value into local storage under a Key like (HR-input).
+        })
+    })
 })
+
+// TODO: On page load, get the local storage saved inputs and display them.
---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://stackoverflow.com/questions/10333230/save-input-data-to-localstorage-on-button-click
